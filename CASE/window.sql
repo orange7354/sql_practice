6ce91f81a1ebf8dbd1d6cf85f11f1d6fbb8b0c50
orange7354 <norisio876@gmail.com>
2024-03-20 16:04:25 +0900

    add window flame SQL

--- CASE/window.sql
+++ CASE/window.sql
@@ -126,3 +126,49 @@ SELECT id,
     price,
     SUM(quantity * price) OVER (ORDER BY date ASC ROWS BETWEEN 2 PRECEDING AND 1 PRECEDING ) as tow_days_total_sales
 FROM sales;
+
+
+-- テーブルの作成
+CREATE TABLE inventory (
+  id INT,
+  date DATE,
+  product VARCHAR(50),
+  quantity INT
+);
+
+-- データの挿入
+INSERT INTO inventory (id, date, product, quantity)
+VALUES
+  (1, '2023-05-01', 'A', 100),
+  (2, '2023-05-02', 'A', 80),
+  (3, '2023-05-03', 'A', 120),
+  (4, '2023-05-04', 'A', 90),
+  (5, '2023-05-05', 'A', 110),
+  (6, '2023-05-06', 'A', 100),
+  (7, '2023-05-07', 'A', 130),
+  (8, '2023-05-08', 'A', 120);
+
+
+
+SELECT 
+    id,
+    date,
+    product,
+    quantity,
+    AVG(quantity) OVER (ORDER BY date ASC BETWEEN 3 PRECEDING AND 1 PRECEDING) as avg_three_dayes_quantity,
+FROM inventory
+ORDER BY date ASC;
+
+-- 以下の要件を満たすSQLクエリを作成してください：
+-- 各行について、その行の日付と、直前の3日間の在庫数の平均値を表示してください。
+-- 結果は、日付の昇順で並べ替えてください。
+
+
+SELECT 
+    id,
+    date,
+    product,
+    quantity,
+    AVG(quantity) OVER (ORDER BY date ASC ROWS BETWEEN 3 PRECEDING AND 1 PRECEDING) AS avg_three_dayes_quantity
+FROM inventory
+ORDER BY date;
