fe8b2b857b3b1938f2b8f59cee429fadffb9dfd5
orange7354 <norisio876@gmail.com>
2024-03-20 15:56:38 +0900

    add window flame SQL

--- CASE/window.sql
+++ CASE/window.sql
@@ -85,8 +85,44 @@ SELECT id,
     name,
     category,
     price,
-    ROW_NUMBER() OVER (PARTITION BY category ORDER BY ASC) as product_rank
+    RANK() OVER (PARTITION BY category ORDER BY price ASC) as product_rank
 FROM products
 ORDER BY
     category ASC,
-    product_rank ASC,
+    product_rank ASC;
+
+-- テーブルの作成
+CREATE TABLE sales (
+  id INT,
+  date DATE,
+  product VARCHAR(50),
+  quantity INT,
+  price DECIMAL(10, 2)
+);
+
+-- データの挿入
+INSERT INTO sales (id, date, product, quantity, price)
+VALUES
+    (1, '2023-05-01', 'A', 10, 50.00),
+    (2, '2023-05-01', 'B', 5, 100.00),
+    (3, '2023-05-02', 'A', 15, 50.00),
+    (4, '2023-05-02', 'C', 8, 75.00),
+    (5, '2023-05-03', 'B', 10, 100.00),
+    (6, '2023-05-03', 'C', 5, 75.00),
+    (7, '2023-05-04', 'A', 20, 50.00),
+    (8, '2023-05-04', 'B', 15, 100.00);
+
+
+--1. 各行について、その行の日付と、直前の2日間の売上合計を表示してください。
+--2. 売上合計は、数量（quantity）と価格（price）を掛け合わせて計算してください。
+--3. 結果は、日付の昇順で並べ替えてください。
+
+
+
+SELECT id,
+    date,
+    product,
+    quantity,
+    price,
+    SUM(quantity * price) OVER (ORDER BY date ASC ROWS BETWEEN 2 PRECEDING AND 1 PRECEDING ) as tow_days_total_sales
+FROM sales;
